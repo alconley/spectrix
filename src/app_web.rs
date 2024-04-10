2ef0e12761ed7c83f1ca1864501e745ce4838a57
alconley <imaconley1@gmail.com>
2024-04-10 07:53:33 -0400

    modified:   src/app_web.rs

--- src/app_web.rs
+++ src/app_web.rs
@@ -1,18 +1,33 @@
 use eframe::App;
 
 #[derive(serde::Deserialize, serde::Serialize, Debug, Default)]
-pub struct EVBApp {}
+pub struct EVBApp {
+    window: bool,
+}
 
 impl EVBApp {
-    pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        Self {}
+    pub fn new(_cc: &eframe::CreationContext<'_>, window: bool) -> Self {
+        Self { window }
     }
 }
 
 impl App for EVBApp {
     fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label("SPS Eventbuilder is not supported in the browser. Please run it natively.");
-        });
+        if self.window {
+            egui::Window::new("SE-SPS Event Builder")
+                .min_width(200.0)
+                .max_width(600.0)
+                .show(ctx, |ui| {
+                    ui.label(
+                        "SPS Eventbuilder is not supported in the browser. Please run it natively.",
+                    );
+                });
+        } else {
+            egui::CentralPanel::default().show(ctx, |ui| {
+                ui.label(
+                    "SPS Eventbuilder is not supported in the browser. Please run it natively.",
+                );
+            });
+        }
     }
 }
