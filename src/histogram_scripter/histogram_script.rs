661498804d18b5b21918a0a23e2bd853a2d1aa8d
alconley <imaconley1@gmail.com>
2025-01-17 10:25:16 -0500

    save/load histogram configs 	modified:   src/histogram_scripter/histogram_script.rs

--- src/histogram_scripter/histogram_script.rs
+++ src/histogram_scripter/histogram_script.rs
@@ -3,6 +3,10 @@ use super::custom_scripts::CustomConfigs;
 use crate::histoer::configs::Configs;
 use crate::histoer::histogrammer::Histogrammer;
 use polars::prelude::*;
+
+use std::fs::File;
+use std::io::{BufReader, Write};
+
 #[derive(Clone, Default, serde::Deserialize, serde::Serialize)]
 pub struct HistogramScript {
     pub configs: Configs,
@@ -17,8 +21,48 @@ impl HistogramScript {
         }
     }
 
+    pub fn save_configs_dialog(&self) {
+        if let Some(path) = rfd::FileDialog::new()
+            .add_filter("JSON Config", &["json"])
+            .set_title("Save Configurations")
+            .save_file()
+        {
+            if let Ok(serialized) = serde_json::to_string_pretty(&self) {
+                if let Ok(mut file) = File::create(path) {
+                    let _ = file.write_all(serialized.as_bytes());
+                }
+            }
+        }
+    }
+
+    pub fn load_configs_dialog(&mut self) {
+        if let Some(path) = rfd::FileDialog::new()
+            .add_filter("JSON Config", &["json"])
+            .set_title("Load Configurations")
+            .pick_file()
+        {
+            if let Ok(file) = File::open(path) {
+                let reader = BufReader::new(file);
+                if let Ok(loaded) = serde_json::from_reader(reader) {
+                    *self = loaded;
+                }
+            }
+        }
+    }
+
     pub fn ui(&mut self, ui: &mut egui::Ui) {
-        ui.heading("Histogram Script");
+        ui.horizontal(|ui| {
+            ui.heading("Histogram Script");
+
+            ui.separator();
+
+            if ui.button("Save Configs").clicked() {
+                self.save_configs_dialog();
+            }
+            if ui.button("Load Configs").clicked() {
+                self.load_configs_dialog();
+            }
+        });
 
         ui.separator();
 
